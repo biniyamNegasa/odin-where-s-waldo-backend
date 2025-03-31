403717515d8d52f9b520c0b91a02684676aea081
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2025-03-31 18:15:51 +0300

    chore: add more coordinates

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,12 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-images_data = [ { name: "beach", waldo: [ 503, 205 ], wenda: [ 629, 218 ], wizard: [ 221, 191 ], odlaw: [ 86, 193 ] } ]
+images_data = [ { name: "beach", waldo: [ 503, 205 ], wenda: [ 629, 218 ], wizard: [ 221, 191 ], odlaw: [ 86, 193 ] },
+                { name: "ice",   waldo: [ 696, 386 ], wenda: [ 398, 223 ], wizard: [ 59, 399 ],  odlaw: [ 257, 334 ] },
+                { name: "olympic", waldo: [ 229, 181 ], wenda: [ 204, 380 ], wizard: [ 499, 456 ], odlaw: [ 487, 341 ] },
+                { name: "carpet", waldo: [ 701, 781 ], wenda: [ 393, 1199 ], wizard: [ 665, 1213 ], odlaw: [ 295, 1422 ] },
+                { name: "space", waldo: [ 329, 340 ], wenda: [ 239, 284 ], wizard: [ 635, 315 ], odlaw: [ 58, 374 ] }
+              ]
 
 images_data.each do | data |
   ImageCoordinate.find_or_create_by!(name: data[:name]) do | image_coordinate |
